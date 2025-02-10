7941ab69a1baa00727a4895c3c977d2591fb4209
Verocrespo <vscdlr@gmail.com>
2025-02-10 20:32:39 +0100

    chore: initial commit create application

---
+++ src/main/java/com/inditex/PriceByTimeAPI/PriceByTimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.PriceByTimeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceByTimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceByTimeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/PriceByTimeAPI/PriceByTimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.PriceByTimeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceByTimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
