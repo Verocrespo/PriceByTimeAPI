92c8ff05e6c6ee9293131ef9c87c6710cfc31988
Verocrespo <vscdlr@gmail.com>
2025-02-11 19:48:27 +0100

    feat: Add h2 repository implementation

---
+++ price-api-infraestructure/src/main/java/com/inditex/price/PriceH2Repository.java
@@ -0,0 +1,22 @@
+package com.inditex.price;
+
+import com.inditex.price.domain.model.PriceCriteria;
+import com.inditex.price.domain.model.Price;
+import com.inditex.price.domain.repository.IPriceRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PriceH2Repository extends JpaRepository<Price, Long>, IPriceRepository {
+
+    @Query("SELECT p " +
+            "FROM Price p "+
+            "WHERE p.productId = :#{#filter.productId} AND p.brandId = :#{#filter.brandId} "+
+            "AND  :#{#filter.applyDate} BETWEEN p.startDate AND p.endDate "
+    )
+    List<Price> findPriceByCriteria(@Param("filter") PriceCriteria priceCriteria);
+}
