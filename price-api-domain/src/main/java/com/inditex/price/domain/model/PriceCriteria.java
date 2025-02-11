9699df81640210bda9310ca1d746c111961bc6ea
Verocrespo <vscdlr@gmail.com>
2025-02-11 19:48:04 +0100

    feat: Add builder to PriceCriteria

--- price-api-domain/src/main/java/com/inditex/price/domain/model/PriceCriteria.java
+++ price-api-domain/src/main/java/com/inditex/price/domain/model/PriceCriteria.java
@@ -1,6 +1,9 @@
 package com.inditex.price.domain.model;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record PriceCriteria (Long productId, Long brandId, LocalDateTime applyDate) {
 }
